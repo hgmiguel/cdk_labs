a393c6de66628eac264be9cd90bb5193d2ad1c6c
Miguel Huerta <miguel.huerta@payclip.com>
2020-03-28 13:13:36 -0600

    Add default region and account

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 
 from aws_cdk import core
 from cdk_labs.s3.bucket_stack import S3BucketStack
+import os
 
 
 app = core.App()
-S3BucketStack(app, "cdk-labs")
+S3BucketStack(app, "cdk-labs", env=core.Environment(
+    account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
+    region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"])))
 
 app.synth()
